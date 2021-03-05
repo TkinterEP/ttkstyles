f60225240816b78ca44deba514298e70c1356d51
rdbende <rdbende@gmail.com>
2021-03-05 11:50:19 +0100

    Add ToggleButton to __init__

--- ttkstyles/widgets/__init__.py
+++ ttkstyles/widgets/__init__.py
@@ -4,4 +4,5 @@ License: GNU GPLv3
 Copyright (c) 2021 RedFantom
 """
 from ttkstyles.widgets.switch import SwitchButton
+from ttkstyles.widgets.toggle import ToggleButton
 from ttkstyles.widgets.tristate import TristateCheckbutton, TristateRadiobutton
